230b2b56e597d68d695babcae7a4eed0659d8fa8
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-28 19:25:18 +0900

    feat: reservationTime jdbcRepository 구현

---
+++ src/main/java/roomescape/time/infra/JdbcReservationTimeRepository.java
@@ -0,0 +1,58 @@
+package roomescape.time.infra;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.time.domain.ReservationTime;
+import roomescape.time.domain.ReservationTimeRepository;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcReservationTimeRepository implements ReservationTimeRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final RowMapper<ReservationTime> rowMapper = (resultSet, rowNum) -> ReservationTime.builder()
+            .id(resultSet.getLong("id"))
+            .startAt(resultSet.getTime("start_at").toLocalTime())
+            .build();
+
+    @Override
+    public ReservationTime save(ReservationTime reservationTime) {
+        String sql = "INSERT INTO reservation_time(start_at) "
+                + "VALUES(:startAt)";
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(reservationTime);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(sql, params, keyHolder);
+        long generatedId = Objects.requireNonNull(keyHolder.getKey()).longValue();
+        reservationTime.setId(generatedId);
+        return reservationTime;
+    }
+
+    @Override
+    public List<ReservationTime> findAll() {
+        String sql = "SELECT id, start_at FROM reservation_time";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    @Override
+    public ReservationTime findById(Long id) {
+        String sql = "SELECT id, start_at FROM reservation_time WHERE id=:id";
+        return jdbcTemplate.queryForObject(sql, Map.of("id", id), rowMapper);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM reservation_time WHERE id=:id";
+        jdbcTemplate.update(sql, Map.of("id", id));
+    }
+}
