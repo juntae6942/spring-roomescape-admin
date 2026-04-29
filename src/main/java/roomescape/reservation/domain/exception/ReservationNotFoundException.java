abc9e2044f512b062b07cea7e4b4ae3403bd953d
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-29 13:58:23 +0900

    refactor: 예외 메서드 수정

--- src/main/java/roomescape/reservation/domain/exception/ReservationNotFoundException.java
+++ src/main/java/roomescape/reservation/domain/exception/ReservationNotFoundException.java
@@ -1,7 +1,7 @@
 package roomescape.reservation.domain.exception;
 
 public class ReservationNotFoundException extends RuntimeException {
-    public ReservationNotFoundException() {
-        super("존재하지 않는 예약ID 입니다.");
+    public ReservationNotFoundException(String message) {
+        super(message);
     }
 }
