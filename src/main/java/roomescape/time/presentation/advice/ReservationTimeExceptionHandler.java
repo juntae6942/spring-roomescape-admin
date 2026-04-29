4d954bfaefaaf51248e10971cc448d5f1952a703
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-29 15:01:46 +0900

    refactor: 예외 처리 수정

--- src/main/java/roomescape/time/presentation/advice/ReservationTimeExceptionHandler.java
+++ src/main/java/roomescape/time/presentation/advice/ReservationTimeExceptionHandler.java
@@ -5,18 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.time.domain.exception.ReservationTimeInUseException;
-import roomescape.reservation.domain.exception.ReservationNotFoundException;
+import roomescape.time.domain.exception.ReservationTimeNotFoundException;
 
 @RestControllerAdvice
 public class ReservationTimeExceptionHandler {
 
-    @ExceptionHandler(ReservationNotFoundException.class)
-    public ResponseEntity<String> handleReservationNotFoundException(ReservationNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
     @ExceptionHandler(ReservationTimeInUseException.class)
     public ResponseEntity<String> handleReservationTimeInUseException(ReservationTimeInUseException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ReservationTimeNotFoundException.class)
+    public ResponseEntity<String> handleReservationTimeNotFoundException(ReservationTimeNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
