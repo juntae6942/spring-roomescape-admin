7b9e05220e248417ae04434efc9bb24c412a6f42
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-29 22:20:03 +0900

    refactor: equals와 hashcode 정의

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -3,12 +3,14 @@ package roomescape.reservation.domain;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import roomescape.time.domain.ReservationTime;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 @AllArgsConstructor
 public class Reservation {
 
--- src/main/java/roomescape/time/domain/ReservationTime.java
+++ src/main/java/roomescape/time/domain/ReservationTime.java
@@ -2,11 +2,13 @@ package roomescape.time.domain;
 
 import java.time.LocalTime;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 public class ReservationTime {
     @Setter
     private Long id;
