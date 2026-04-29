40f6a09c41ce559b9478605dde95c4b09c9634a3
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-29 14:58:03 +0900

    test: 응답값 수정에 따른 Http 상태 변경

--- src/test/java/roomescape/MissionStepTest3.java
+++ src/test/java/roomescape/MissionStepTest3.java
@@ -24,7 +24,7 @@ public class MissionStepTest3 {
                 .body(params)
                 .when().post("/times")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .when().get("/times")
@@ -35,7 +35,7 @@ public class MissionStepTest3 {
         RestAssured.given().log().all()
                 .when().delete("/times/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
     }
 
     @Test
@@ -48,7 +48,7 @@ public class MissionStepTest3 {
                 .body(params)
                 .when().post("/times")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
@@ -60,7 +60,7 @@ public class MissionStepTest3 {
                 .body(reservation)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
